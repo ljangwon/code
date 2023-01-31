2ebae7fdcb39bacc5261665cce165bbf03c851d1
jakeleanco <jakeleanco@gmail.com>
2023-01-31 12:00:44 +0900

    init

--- ci/application/config/config.php
+++ ci/application/config/config.php
@@ -27,6 +27,8 @@ $config['base_url'] = 'http://code.leanedu22.site/';
 //$config['base_url'] = 'http://leanmath.leanedu22.site/';
 //$config['base_url'] = 'http://jakeleanco.ipdisk.co.kr:8000/';
 
+
+
 /*
 |--------------------------------------------------------------------------
 | Index File
