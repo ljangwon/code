eaa74024433fe42395cecc3fe2a484d836c51daa
jakeleanco <jakeleanco@gmail.com>
2023-01-31 12:02:47 +0900

    init

--- ci/application/config/template/config.php
+++ ci/application/config/template/config.php
@@ -23,7 +23,10 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
+
 $config['base_url'] = 'http://code.leanedu22.site/';
+//$config['base_url'] = 'http://leanmath.leanedu22.site/';
+//$config['base_url'] = 'http://jakeleanco.ipdisk.co.kr:8000/';
 
 /*
 |--------------------------------------------------------------------------
